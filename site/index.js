e9c4763ff402a731d83f842f0913592ee6c153fb
lieutenantX <49363368+lieutenantX@users.noreply.github.com>
2021-03-23 15:29:35 +0100

    Update index.js

--- site/index.js
+++ site/index.js
@@ -1,1 +1,5 @@
-
+function myFunction() {
+  var link = "https://discord.com/api/v8/invites/Umw5NxcVSP?with_counts=true"; 
+  var res = fetch(link).json()
+  document.getElementById("demo").innerHTML = res;
+}
